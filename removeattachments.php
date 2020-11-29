1838111cf81c19b1f4c8a01d0835b552d39cf5dd
dsoares <diana.soares@gmail.com>
2020-11-29 22:06:33 +0000

    Update license info.

--- removeattachments.php
+++ removeattachments.php
@@ -1,7 +1,24 @@
 <?php
-
 /**
- * Roundcube plugin RemoveAttachments.
+ * Roundcube Plugin RemoveAttachments.
+ *
+ * Copyright (C) Diana Soares
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (with exceptions
+ * for skins & plugins) as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+/**
+ * Roundcube plugin removeattachments.
  *
  * Roundcube plugin to allow the removal of attachments from a message.
  * Original code from Philip Weir.
